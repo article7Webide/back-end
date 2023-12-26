264c96c9fee8912efaba68b38edda3706514ce15
YoonjeongChoi <gkt0103@daum.net>
2023-12-26 22:07:14 +0900

    CI/CD 설정파일

--- src/test/java/article/WebIde/WebIdeApplicationTests.java
+++ src/test/java/article/WebIde/WebIdeApplicationTests.java
@@ -3,7 +3,7 @@ package article.WebIde;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class WebIdeApplicationTests {
 
 	@Test
