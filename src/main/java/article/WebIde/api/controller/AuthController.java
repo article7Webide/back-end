243aba2bc2835bba418eceb2061f2ad83b7bba5f
JeYoung Park <wpdud0713@naver.com>
2023-12-27 20:31:28 +0900

    [feat] 이메일 유효성 검사 추가

--- src/main/java/article/WebIde/api/controller/AuthController.java
+++ src/main/java/article/WebIde/api/controller/AuthController.java
@@ -5,6 +5,8 @@ import article.WebIde.api.dto.MemberRequestDto;
 import article.WebIde.api.dto.MemberResponseDto;
 import article.WebIde.api.dto.TokenDto;
 import article.WebIde.api.dto.TokenRequestDto;
+import article.WebIde.api.dto.util.EmailValidator;
+import article.WebIde.api.dto.util.InvalidEmailException;
 import article.WebIde.api.dto.util.InvalidPasswordException;
 import article.WebIde.api.dto.util.PasswordValidator;
 import article.WebIde.api.service.AuthService;
@@ -20,15 +22,23 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins="*", allowedHeaders = "*")
 public class AuthController {
     private final AuthService authService;
+    private final EmailValidator emailValidator;
 
     @PostMapping("/signup")
     public ResponseEntity<Object> signup(@RequestBody MemberRequestDto memberRequestDto) {
         try {
+            // 이메일 유효성 검사
+            if (!emailValidator.isValidEmail(memberRequestDto.getEmail())) {
+                throw new InvalidEmailException("유효한 이메일 주소를 입력하세요.");
+            }
+
             // 비밀번호 유효성 검사
             PasswordValidator.validatePassword(memberRequestDto.getPassword());
 
             ResponseEntity<MemberResponseDto> response = authService.signup(memberRequestDto);
             return ResponseEntity.ok(response.getBody());
+        } catch (InvalidEmailException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
         } catch (InvalidPasswordException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
         } catch (RuntimeException e) {
