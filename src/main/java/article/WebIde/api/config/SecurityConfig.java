b36eac2f5607f5377240bd0f1eee58757a75e0ec
YoonjeongChoi <gkt0103@daum.net>
2023-12-28 12:35:30 +0900

    permitAll 설정 수정

--- src/main/java/article/WebIde/api/config/SecurityConfig.java
+++ src/main/java/article/WebIde/api/config/SecurityConfig.java
@@ -62,6 +62,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/**").permitAll()
                 .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
